d3c92d2f9bb18afc02d66fa5cda2b340e54a0840
Robzlegz <robzlegz@gmail.com>
2021-03-01 13:04:00 +0200

    outline for project

--- src/components/SidebarLeft.js
+++ src/components/SidebarLeft.js
@@ -9,6 +9,7 @@ import BookmarkBorderOutlinedIcon from '@material-ui/icons/BookmarkBorderOutline
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { Avatar, Button } from '@material-ui/core';
 
 const SidebarLeft = () => {
     return (
@@ -18,11 +19,48 @@ const SidebarLeft = () => {
             </StyledLeftSidebarHeader>
             <StyledLeftSidebarLinks>
                 <StyledLeftSidebarLink>
-
+                    <HomeOutlinedIcon />
+                    <h4>Home</h4>
                 </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <AlternateEmailIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <NotificationsNoneOutlinedIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <EmailOutlinedIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>                
+                <StyledLeftSidebarLink>
+                    <BookmarkBorderOutlinedIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <ListAltOutlinedIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <PersonOutlineOutlinedIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <StyledLeftSidebarLink>
+                    <MoreHorizIcon />
+                    <h4>Home</h4>
+                </StyledLeftSidebarLink>
+                <Button>Tweet</Button>
             </StyledLeftSidebarLinks>
             <StyledLeftSidebarProfile>
-
+                <Avatar />
+                <div className="profile__middle">
+                    <strong><p>Rob</p></strong>
+                    <small><p>@Robzlegz</p></small>
+                </div>
+                <div className="profile__left">
+                    <MoreHorizIcon />
+                </div>
             </StyledLeftSidebarProfile>
         </StyledLeftSidebar>
     )
@@ -34,7 +72,7 @@ const StyledLeftSidebarHeader = styled.div`
 
 `;
 const StyledLeftSidebarLinks = styled.div`
-    
+    color:#1DA1F2;
 `;
 const StyledLeftSidebarLink = styled.div`
     
